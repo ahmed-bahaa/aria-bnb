15e520662516774aa1b65483b9cd3a9e22a01658
ahmed_bahaa <a.bahaarefaii@gmail.com>
2022-05-15 16:02:16 +0200

    addgin server side validation III

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,10 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Valid returns true if there no erros, otherwise return false
@@ -32,4 +34,23 @@ func (f *Form) Has(field string, r *http.Request) bool {
 	}
 	f.Errors.Add(field, "this field cannot be blank")
 	return false
+}
+
+// Required check for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field Cannot be Blank")
+		}
+	}
+}
+
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
+		return false
+	}
+	return true
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -102,8 +102,14 @@ func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+
 	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
 		Form: forms.New(nil),
+		Data: data,
 	})
 }
 
@@ -118,13 +124,15 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
-		Email:     r.Form.Get("phone"),
-		Phone:     r.Form.Get("email"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
 	}
 
 	form := forms.New(r.PostForm)
 
-	form.Has("first_name", r)
+	// form.Has("first_name", r)
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 5, r)
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
